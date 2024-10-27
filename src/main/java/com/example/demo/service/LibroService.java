6acf4c2c81d11b46557d0cadea874761375946be
Yoel <yoelgarcialago@gmail.com>
2024-10-27 16:55:01 +0100

    modificado service + añadido método de prueba

--- src/main/java/com/example/demo/service/LibroService.java
+++ src/main/java/com/example/demo/service/LibroService.java
@@ -1,14 +1,29 @@
 package com.example.demo.service;
 
 import com.example.demo.models.Libros;
+import com.example.demo.repository.LibrosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 @Service
 public class LibroService {
 
+    LibrosRepository librosRepository;
+
+    @Autowired
+    public LibroService(LibrosRepository librosRepository){
+        this.librosRepository = librosRepository;
+    }
+
 public Libros mostrarCoso(){
-    return new Libros(0,"wawa", Date.valueOf("2024-10-20"),"xd");
+    return new Libros("ISBN","name", Date.valueOf(LocalDate.now()),"wawa",Date.valueOf(LocalDate.now()),0);
+   // return null;
 }
+
+    public void addingNewLibros(Libros libro){
+        System.out.println(libro);
+    }
 }
