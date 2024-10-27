910f38b73ecad0e99399b8128ba791fd08219e00
Yoel <yoelgarcialago@gmail.com>
2024-10-27 20:18:59 +0100

    Creado el comienzo de la view

---
+++ src/main/java/com/example/proyectoAD/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.proyectoAD.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+@GetMapping("/")
+    public String index(Model model){
+    model.addAttribute("Wellcome","Inicio");
+    return "index";
+}
+}
